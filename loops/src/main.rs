62707c52d82a981fc2fc76166f0594fda88256b9
Zak Johnson <fleecemane.lyons@icloud.com>
2019-12-03 15:34:34 -0800

    loops example 2

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,6 +1,13 @@
 fn main() {
-    loop {
-        println!("Again!");
-    }
-    println!("Hello, world!");
+
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+    println!("the result is {}", result);
 }

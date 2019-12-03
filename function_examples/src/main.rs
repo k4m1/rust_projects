3600e4f767a040ec4e916c61a55e04a8b810fb0f
Zak Johnson <fleecemane.lyons@icloud.com>
2019-12-03 13:07:53 -0800

    more examples

--- function_examples/src/main.rs
+++ function_examples/src/main.rs
@@ -2,8 +2,27 @@ fn main() {
     println!("Hello, world!");
 
     example_function();
+    example_with_params(69);
+    // exmaple_with_multiparams(42, "howdy");
+    let t = example_with_return_label();
+    println!("the value of t is {}", t);
 }
 
 fn example_function() {
     println!("TFOT That function over there");
 }
+
+fn example_with_params(x: i32) {
+    println!("the value of x is {}", x);
+}
+
+// fn exmaple_with_multiparams(x: i32, y: String) {
+//     println!("the value of x is {} the string y is {}", x, y);
+// }
+
+fn example_with_return_label() -> i32 {
+    420
+}
+
+
+

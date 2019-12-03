6c87233343be9744344eaf2f6fa990ab0ee74570
Zak Johnson <fleecemane.lyons@icloud.com>
2019-12-03 15:36:51 -0800

    example 3

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,13 +1,10 @@
 fn main() {
 
-    let mut counter = 0;
+    let mut number = 3;
 
-    let result = loop {
-        counter += 1;
-
-        if counter == 10 {
-            break counter * 2;
-        }
-    };
-    println!("the result is {}", result);
+    while number != 0 {
+        println!("the number is {}", number);
+        number -= 1;
+    }
+    println!("Lift off!");
 }
